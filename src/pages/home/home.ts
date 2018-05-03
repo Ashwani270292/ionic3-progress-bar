270e0c05f2994fe814cf815d1fa1de10b95ab49a
Ashwani <ashwani@edcams.com>
2018-05-03 12:23:30 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  charcterstic1: number;
+  charcterstic2: number;
+  charcterstic3: number;
+  charcterstic4: number;
+  charcterstic5: number;
+
+  charName1: string;
+  charName2: string;
+  charName3: string;
+  charName4: string;
+  charName5: string;
+
+
+  dataList: any;
   constructor(public navCtrl: NavController) {
+      this.charcterstic1 = 50;
+      this.charcterstic2 = 30;
+      this.charcterstic3 = 70;
+      this.charcterstic4 = 80;
+      this.charcterstic5 = 90;
+
+      this.charName1 = 'Enthusiast';
+      this.charName2 = 'Enthusiast';
+      this.charName3 = 'Enthusiast';
+      this.charName4 = 'Enthusiast';
+      this.charName5 = 'Enthusiast';
+
+      this.dataList = [];
+
+      this.dataList.push('Hi this is a dummy feedback which is to test how it will look in app');
+      this.dataList.push('Hi this is a dummy feedback which is to test how it will look in app');
+      this.dataList.push('Hi this is a dummy feedback which is to test & how it will look in app');
+      this.dataList.push('Hi this is a dummy feedback which is to test how it will look in app');
+      this.dataList.push('Hi this is a dummy feedback which is to test how it will look in app');
+      this.dataList.push('Hi this is a dummy feedback which is to test how it will look in app');
 
   }
 
